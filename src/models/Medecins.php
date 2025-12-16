30c634d432becc2a7c8987f015da2809dec12555
kadiri-mohamed <med2004kad@gmail.com>
2025-12-16 15:02:03 +0100

    HL-34 #done Add Medecin model with getAll and getById methods

---
+++ src/models/Medecins.php
@@ -0,0 +1,30 @@
+<?php
+require_once __DIR__ . '/../config/Database.php';
+
+class Medecin {
+    private $conn;
+    
+    public function __construct() {
+        $this->conn = Database::getInstance();
+    }
+    
+    public function getAll() {
+        $sql = "SELECT m.*, d.nom as department_nom 
+                FROM medecins m 
+                LEFT JOIN departments d ON m.department_id = d.id 
+                ORDER BY m.created_at DESC";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
+    public function getById($id) {
+        $sql = "SELECT m.*, d.nom as department_nom 
+                FROM medecins m 
+                LEFT JOIN departments d ON m.department_id = d.id 
+                WHERE m.id = :id";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
